7a320b0f1c956f38b2e6623a1321a4116d53c84d
AspirantDrago <sanekclubstr@mail.ru>
2026-06-30 16:59:30 +0500

    Set single-instance

--- apps/CmncWanWidget/App.xaml.cs
+++ apps/CmncWanWidget/App.xaml.cs
@@ -1,5 +1,58 @@
-﻿namespace CmncWanWidget;
+﻿using System.Threading;
+using System.Windows;
+
+namespace CmncWanWidget;
 
 public partial class App
 {
+    private const string MutexName = @"Global\ClassroomNetControl.CmncWanWidget";
+
+    private Mutex? _singleInstanceMutex;
+    private bool _ownsMutex;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        try
+        {
+            _singleInstanceMutex = new Mutex(
+                initiallyOwned: true,
+                name: MutexName,
+                createdNew: out var createdNew
+            );
+
+            _ownsMutex = createdNew;
+
+            if (!createdNew)
+            {
+                Shutdown(0);
+                return;
+            }
+        }
+        catch
+        {
+            Shutdown(1);
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_ownsMutex)
+        {
+            try
+            {
+                _singleInstanceMutex?.ReleaseMutex();
+            }
+            catch
+            {
+                // Игнорируем ошибку освобождения mutex при завершении приложения.
+            }
+        }
+
+        _singleInstanceMutex?.Dispose();
+
+        base.OnExit(e);
+    }
 }
